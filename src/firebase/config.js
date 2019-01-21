2369caf960c8685a319a01f279d1a0c25c366925
CSNatLUTHER <46822239+CSNatLUTHER@users.noreply.github.com>
2019-01-21 14:27:41 -0600

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
-import api_key from './api_key';
 
 // Initialize Firebase
 var config = {
-   apiKey: api_key,
-   authDomain: "quotesify-421ca.firebaseapp.com",
-   databaseURL: "https://quotesify-421ca.firebaseio.com",
-   projectId: "quotesify-421ca",
-   storageBucket: "quotesify-421ca.appspot.com",
-   messagingSenderId: "677878174342" };
+    apiKey: "AIzaSyA1CEbnYCvo_HIgE8tmeuTXqMisfhdkQEY",
+    authDomain: "quotesify-421ca.firebaseapp.com",
+    databaseURL: "https://quotesify-421ca.firebaseio.com",
+    projectId: "quotesify-421ca",
+    storageBucket: "quotesify-421ca.appspot.com",
+    messagingSenderId: "677878174342"
+  };
  
  firebase.initializeApp(config);
  const storage = firebase.storage();
