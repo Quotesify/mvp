c81c26fb71693e6c785099b6508f9ac005ef9bae
Jessica Stephens <jeslstep1@gmail.com>
2019-01-21 14:37:51 -0600

    ready for deploying to heroku

---
+++ build/precache-manifest.b794d9f2ce236c290a3f2afbce935010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d731b5436f6c42b9a174",
+    "url": "/static/js/main.d731b543.chunk.js"
+  },
+  {
+    "revision": "0b08031078b02dfe1f24",
+    "url": "/static/js/1.0b080310.chunk.js"
+  },
+  {
+    "revision": "d731b5436f6c42b9a174",
+    "url": "/static/css/main.ebc3406f.chunk.css"
+  },
+  {
+    "revision": "c19df5f5a5aa086c6b739674a3a674b8",
+    "url": "/index.html"
+  }
+];
