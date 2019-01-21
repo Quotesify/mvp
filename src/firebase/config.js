c45264b041f0bac691b92e779993650e31b5cc2b
Malik-G <Malik-G@users.noreply.github.com>
2019-01-21 00:13:51 -0600

    api key edit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,11 @@ import api_key from './api_key';
 // Initialize Firebase
 var config = {
    apiKey: api_key,
-   authDomain: "traust-poc.firebaseapp.com",
-   databaseURL: "https://traust-poc.firebaseio.com",
-   projectId: "traust-poc",
-   storageBucket: "traust-poc.appspot.com",
-   messagingSenderId: "159349695299"
- };
+   authDomain: "quotesify-421ca.firebaseapp.com",
+   databaseURL: "https://quotesify-421ca.firebaseio.com",
+   projectId: "quotesify-421ca",
+   storageBucket: "quotesify-421ca.appspot.com",
+   messagingSenderId: "677878174342" };
  
  firebase.initializeApp(config);
  const storage = firebase.storage();
